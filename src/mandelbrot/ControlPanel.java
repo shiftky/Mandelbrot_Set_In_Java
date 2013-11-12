8272a1a05125f96bc5ce6e79c943154209cad818
shiftky <shiftky@gmail.com>
2013-11-12 12:07:38 +0900

    拡大時の動作を変更 & 拡大枠の中心に印をつけた

--- src/mandelbrot/ControlPanel.java
+++ src/mandelbrot/ControlPanel.java
@@ -13,7 +13,10 @@ public class ControlPanel extends JPanel {
 	private EnlargeListener enlargeListener = null;
 	private MouseMovedListener mouseMovedListener = null;
 	private int width, height;
+	private int mouse_x1, mouse_x2, mouse_y1, mouse_y2;
 	private int x1, x2, y1, y2;
+	private int start_x, start_y;
+	private int old_mouse_y = -1;
 
 	public ControlPanel(int w, int h){
 		width = w;
@@ -35,11 +38,37 @@ public class ControlPanel extends JPanel {
 
 	private void initMousePoint(){
 		x1 = y1 = x2 = y2 = -1;
+		mouse_x1 = mouse_x2 = mouse_y1 = mouse_y2 = -1;
+		start_x = start_y = old_mouse_y = -1;
 	}
 
 	public void paintComponent(Graphics g){
+		if(mouse_x1 > mouse_x2 && mouse_y1 > mouse_y2) {
+			x1 = mouse_x2;
+			x2 = mouse_x1;
+			y1 = mouse_y2;
+			y2 = mouse_y1;
+		} else if(mouse_x1 > mouse_x2) {
+			x1 = mouse_x2;
+			x2 = mouse_x1;
+			y1 = mouse_y1;
+			y2 = mouse_y2;
+		} else if(mouse_y1 > mouse_y2) {
+			x1 = mouse_x1;
+			x2 = mouse_x2;
+			y1 = mouse_y2;
+			y2 = mouse_y1;
+		} else {
+			x1 = mouse_x1;
+			x2 = mouse_x2;
+			y1 = mouse_y1;
+			y2 = mouse_y2;
+		}
+
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.setColor(Color.white);
+		g2d.drawLine(start_x-5, start_y, start_x+5, start_y);
+		g2d.drawLine(start_x, start_y-5, start_x, start_y+5);
 		g2d.drawRect(x1, y1 , y2 - y1, y2 - y1);
 	}
 
@@ -49,16 +78,35 @@ public class ControlPanel extends JPanel {
 		}
 
 		public void mouseDragged(MouseEvent e){
-			x2 = e.getX();
-			y2 = e.getY();
-			repaint();
+			 int up;
+			 if( old_mouse_y == -1 ) {
+				 old_mouse_y = e.getY();
+				 return;
+			 }
+			 if( old_mouse_y > e.getY() ) {
+				 up = e.getY() - old_mouse_y;
+				 mouse_x1 += up;
+				 mouse_x2 -= up;
+				 mouse_y1 += up;
+				 mouse_y2 -= up;
+				 repaint();
+			 } else if( old_mouse_y < e.getY() ) { 
+				 up = old_mouse_y - e.getY();
+				 mouse_x1 -= up;
+				 mouse_x2 += up;
+				 mouse_y1 -= up;
+				 mouse_y2 += up;
+				 repaint();
+			 }
+			 old_mouse_y = e.getY();
+			 repaint();
 		}
 	}
 
 	class inMouseListener extends MouseAdapter{
 		public void mousePressed(MouseEvent e){
-			x1 = e.getX();
-			y1 = e.getY();
+			start_x = mouse_x1 = mouse_x2 = e.getX();
+			start_y = mouse_y1 = mouse_y2 = e.getY();
 			repaint();
 		}
 		
