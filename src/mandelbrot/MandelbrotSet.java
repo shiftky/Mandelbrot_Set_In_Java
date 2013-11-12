3d66da55e39b4f8e11f00ac9c08d79446be10153
shiftky <shiftky@gmail.com>
2013-11-12 09:21:01 +0900

    buffer を使って描画処理を高速化した

--- src/mandelbrot/MandelbrotSet.java
+++ src/mandelbrot/MandelbrotSet.java
@@ -4,22 +4,42 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import javax.swing.JPanel;
 
 class MandelbrotSet extends JPanel implements EnlargeListener {
 	public double r1, r2, i1, i2;
 	public int width, height;
+	private BufferedImage buffimg;
+	private Graphics bfg;
 
 	public MandelbrotSet(int w, int h){
 		width = w;
 		height = h;
-
-		setBackground(Color.black);
 		setLayout(new BorderLayout());
 		setPreferredSize(new Dimension(width, height));
-		
+
+		buffimg = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		bfg = buffimg.createGraphics();
+
 		initRange();
+		drawSet();
+	}
+
+	public void drawSet(){
+		bfg.setColor(Color.black);
+		bfg.fillRect(0, 0, width, height);
+		for(int x=0; x<width; x++){
+			for (int y=0; y<height; y++){
+				Complex c = new Complex(Utils.map((double) x, 0.0, (double) width, r1, r2),
+									    Utils.map((double) y, 0.0, (double) height, i1, i2));
+				bfg.setColor(new Color(102, 153, 255));
+				if (mandel(c, 30) == true){
+			        bfg.fillRect(x, y, 1, 1);
+				}
+			}
+		}	
+		repaint();
 	}
 
 	public void initRange(){
@@ -29,7 +49,11 @@ class MandelbrotSet extends JPanel implements EnlargeListener {
 
 	public void reset() {
 		initRange();
-		repaint();
+		drawSet();
+	}
+
+	public void paintComponent(Graphics g){
+	    g.drawImage(buffimg, 0, 0, this);
 	}
 
 	public void changeDrawingArea(int x1, int x2, int y1, int y2) {
@@ -40,30 +64,11 @@ class MandelbrotSet extends JPanel implements EnlargeListener {
 			double tmp_i2 = Utils.map((double) y2, 0.0, (double) height, i1, i2);
 			r1 = tmp_r1; r2 = tmp_r2;
 			i1 = tmp_i1; i2 = tmp_i2;
-			repaint();
+			drawSet();
 		}
 	}
-	
-	public void paintComponent(Graphics g){
-	    super.paintComponent(g);
-	    draw((Graphics2D) g);
-	}
 
-	public void draw(Graphics2D g2d){
-		for(int x=0; x<width; x++){
-			for (int y=0; y<height; y++){
-				Complex c = new Complex(Utils.map((double) x, 0.0, (double) width, r1, r2),
-									    Utils.map((double) y, 0.0, (double) height, i1, i2));
-
-				if (mandel(c, 30) == true){
-					g2d.setColor(new Color(102, 153, 255));
-					g2d.drawLine(x, y, x, y);
-				}
-			}
-		}	
-	}
-
-	private boolean mandel(Complex z, int max){
+	public boolean mandel(Complex z, int max){
 		int score = 0;
 		Complex c = new Complex(z.re, z.im);
 
@@ -77,6 +82,4 @@ class MandelbrotSet extends JPanel implements EnlargeListener {
 		}
 		return false;
 	}
-
-
 }
